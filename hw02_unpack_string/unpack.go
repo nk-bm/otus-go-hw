cb5d8e30283b1dac0e19afad0c4974e0a659fefe
Nikita Bamba <bnik889@gmail.com>
2024-02-27 14:13:32 +0300

    hw 2 done

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	slen := len(str)
+	var res string
+
+	var shield bool
+	var buf string
+
+	for i := 0; i < slen; i++ {
+		currentSymbol := str[i]
+		if currentSymbol == 92 && !shield {
+			shield = true
+			continue
+		}
+
+		if currentSymbol >= '0' && currentSymbol <= '9' {
+			if !shield {
+				return "", ErrInvalidString
+			}
+			buf = strconv.Itoa(int(currentSymbol - 48))
+		} else {
+			if shield && currentSymbol != 92 {
+				buf = "\\" + string(currentSymbol)
+			} else {
+				buf = string(currentSymbol)
+			}
+		}
+
+		shield = false
+		if i+1 < slen {
+			if repeatCount, err := strconv.Atoi(string(str[i+1])); err == nil {
+				if repeatCount > 0 {
+					res += strings.Repeat(buf, repeatCount)
+				}
+				i++
+				continue
+			}
+		}
+
+		res += string(currentSymbol)
+	}
+	return res, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,14 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\\\3\`, expected: `qwe\3`},
+		{input: ``, expected: ``},
+		{input: `d\n5abc`, expected: `d\n\n\n\n\nabc`},
+		{input: `aaa0b`, expected: `aab`},
 	}
 
 	for _, tc := range tests {
